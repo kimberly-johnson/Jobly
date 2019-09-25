dcc53d99498b475a5ff026ba4ac600305099d544
Kimberly Johnson <kajohnson3140@gmail.com>
2019-09-25 13:18:44 -0700

    navbar

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -5,15 +5,15 @@ import "./Navbar.css";
 class Navbar extends Component {
   render() {
     return (
-      <div className="navbar">
-        <ul className="navbar">
-          <li><NavLink exact to="/">Jobly</NavLink></li>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <button className="navbar-brand btn btn-primary-outline"><NavLink exact to="/">Jobly</NavLink></button>
+        <ul className="navbar-nav ml-auto">
           <li><NavLink exact to="/companies">Companies</NavLink></li>
           <li><NavLink exact to="/jobs">Jobs</NavLink></li>
           <li><NavLink exact to="/profile">Profile</NavLink></li>
           <li><NavLink exact to="/">Log out</NavLink></li>
         </ul>
-      </div>
+      </nav>
     );
   }
 }
