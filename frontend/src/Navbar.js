6d5b824019be5b3b8b0fb4627a5191f20014fb6f
Chris Martin <chris@sensedev.com>
2019-09-27 16:38:09 -0700

    nav active links

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -4,15 +4,52 @@ import "./Navbar.css";
 
 class Navbar extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      companyActive: '',
+      jobsActive: '',
+      profileActive: ''
+    }
+
+    this.handleCurrent = this.handleCurrent.bind(this);
+  }
+
+  handleCurrent(e) {
+    console.log(e.target.href);
+    if (e.target.href.includes('jobs')) {
+      this.setState({
+        companyActive: '',
+        jobsActive: 'current',
+        profileActive: ''
+      })
+    }
+    else if (e.target.href.includes('companies')) {
+      this.setState({
+        companyActive: 'current',
+        jobsActive: '',
+        profileActive: ''
+      })
+    }
+    else if (e.target.href.includes('profile')) {
+      this.setState({
+        companyActive: '',
+        jobsActive: '',
+        profileActive: 'current'
+      })
+    }
+  }
+
   render() {
     if (localStorage.getItem('token')) {
       return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <button className="navbar-brand btn btn-primary-outline"><NavLink exact to="/">Jobly</NavLink></button>
+          <button className="navbar-brand btn btn-primary-outline"><NavLink onClick={this.handleCurrent} exact to="/companies">Jobly</NavLink></button>
           <ul className="navbar-nav ml-auto">
-            <li><NavLink exact to="/companies">Companies</NavLink></li>
-            <li><NavLink exact to="/jobs">Jobs</NavLink></li>
-            <li><NavLink exact to="/profile">Profile</NavLink></li>
+            <li><NavLink onClick={this.handleCurrent} className={this.state.companyActive} exact to="/companies">Companies</NavLink></li>
+            <li><NavLink onClick={this.handleCurrent} className={this.state.jobsActive} exact to="/jobs">Jobs</NavLink></li>
+            <li><NavLink onClick={this.handleCurrent} className={this.state.profileActive} exact to="/profile">Profile</NavLink></li>
             <li><span onClick={this.props.logOut}><NavLink exact to="/">Log out</NavLink></span></li>
           </ul>
         </nav>
